fe7351f3d18f78c015689eb326ffb0439c99e2e8
Vincent van Proosdij <vincent.van.proosdij@bridging-it.de>
2016-10-31 14:11:06 +0100

    first commit

---
+++ src/Hangfire.Dashboard.Dark/GlobalConfigurationExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Hangfire.Dashboard;
+using Hangfire.Dashboard.Extensions;
+using System;
+using System.Reflection;
+
+namespace Hangfire.Console
+{
+    /// <summary>
+    /// Provides extension methods to setup Hangfire.Dashboard.Dark
+    /// </summary>
+    public static class GlobalConfigurationExtensions
+    {
+        /// <summary>
+        /// Configures Hangfire to use Console.
+        /// </summary>
+        /// <param name="configuration">Global configuration</param>
+        public static IGlobalConfiguration UseDarkDashboard(this IGlobalConfiguration configuration)
+        {
+            if (configuration == null)
+                throw new ArgumentNullException(nameof(configuration));
+
+            // register dispatchers for CSS
+            var assembly = typeof(GlobalConfigurationExtensions).GetTypeInfo().Assembly;
+            DashboardRoutes.Routes.Append("/css[0-9]{3}", new EmbeddedResourceDispatcher(assembly, "Hangfire.Console.Resources.style.css"));
+
+            return configuration;
+        }
+    }
+}
