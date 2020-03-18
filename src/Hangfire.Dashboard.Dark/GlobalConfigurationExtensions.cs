d2556099d4d18931a319efe957190cc4ae9b496c
Sebastian Segerer <sebastian.segerer@me.com>
2020-03-18 17:57:24 +0100

    Fixed Route pathTemplate to match Hangfire's definition Updated appveyor.yml to use VS 2019 Updated Nuget license tag

--- src/Hangfire.Dashboard.Dark/GlobalConfigurationExtensions.cs
+++ src/Hangfire.Dashboard.Dark/GlobalConfigurationExtensions.cs
@@ -20,7 +20,7 @@ namespace Hangfire.Dashboard.Dark
 
             // register dispatchers for CSS
             var assembly = typeof(GlobalConfigurationExtensions).GetTypeInfo().Assembly;
-            DashboardRoutes.Routes.Append("/css[0-9]{3}", new EmbeddedResourceDispatcher(assembly, "Hangfire.Dashboard.Dark.Resources.style.min.css"));
+            DashboardRoutes.Routes.Append("/css[0-9]+", new EmbeddedResourceDispatcher(assembly, "Hangfire.Dashboard.Dark.Resources.style.min.css"));
 
             return configuration;
         }
